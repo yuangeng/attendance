216deb6b3e133f8e377d00b19f0ecb2237454976
vagrant <vagrant@kellogg-dev-box>
2014-04-19 18:24:50 +0000

    Generate the app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Attendance::Application.config.secret_key_base = '338ba7eae5ac0bae29d642a5d950950814d84550b2ff17efcfccc298c5226893384e86f43d769681d7d064a88e729e5618d0a7f56f731aed979a335168c13a38'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Attendance::Application.config.session_store :cookie_store, key: '_attendance_session'
