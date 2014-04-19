fdcf744172306dadfee107f6b3f9e81e721284cc
vagrant <vagrant@kellogg-dev-box>
2014-04-19 21:19:23 +0000

    Start the roll call page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Attendance::Application.routes.draw do
+  get '/rollcall' => 'students/list'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
